75c0850bf235496661d511c630be7c78bb1bb6c4
Markus Legner <markus@mystenlabs.com>
2023-11-22 18:17:48 +0100

    test(daemon): add simple integration tests

---
+++ crates/scion/tests/test_daemon.rs
@@ -0,0 +1,40 @@
+use std::{env, str::FromStr};
+
+use scion::daemon::DaemonClient;
+use scion_proto::address::IsdAsn;
+
+const DEFAULT_DAEMON_ADDRESS: &str = "localhost:30255";
+const DEFAULT_DESTINATION_ISD_ASN: &str = "1-ff00:0:112";
+
+async fn daemon_client() -> DaemonClient {
+    let mut address = env::var("DAEMON_ADDRESS").unwrap_or(DEFAULT_DAEMON_ADDRESS.into());
+    if !address.contains("://") {
+        address = format!("http://{}", address);
+    }
+    DaemonClient::connect(&address)
+        .await
+        .expect("should be able to connect")
+}
+
+#[tokio::test]
+#[ignore = "requires running sciond"]
+async fn daemon_request_sas_info() {
+    let client = daemon_client().await;
+    assert!(client.local_sas_info().await.is_ok())
+}
+
+#[tokio::test]
+#[ignore = "requires running sciond"]
+async fn daemon_request_paths() {
+    let destination_isd_asn =
+        env::var("DESTINATION_ISD_ASN").unwrap_or(DEFAULT_DESTINATION_ISD_ASN.into());
+    let paths = daemon_client()
+        .await
+        .paths_to(IsdAsn::from_str(&destination_isd_asn).expect("correct ISD-ASN"))
+        .await;
+
+    assert!(paths.is_ok());
+    // This could be a bit brittle in case sciond doesn't actually have any paths.
+    // However, in a local topology, this should not be an issue.
+    assert!(paths.unwrap().count() > 0);
+}
