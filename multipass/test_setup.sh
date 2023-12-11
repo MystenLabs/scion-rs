9a574bdd39f7f072121f730b948f2ff9c64bebbd
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2023-12-11 18:55:00 +0100

    feat: add cloudconfig for local vm deployment

---
+++ multipass/test_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# This script assumes you have set up multipass as described in CONTRIBUTING.md
+# and the SCION VM is running.
+#
+# Usage: . ./test_setup.sh
+
+VM_ADDRESS=$(multipass info scion | awk '/IPv4/{print $2}')
+
+export SCION_DISPATCHER_PATH=/tmp/dispatcher.sock
+export SCION_DAEMON_ADDRESS="[$VM_ADDRESS]:30255"
+
+rm -f $SCION_DISPATCHER_PATH
+ssh -i multipass/test_id_ed25519 ubuntu@$VM_ADDRESS -fN -L $SCION_DISPATCHER_PATH:/run/shm/dispatcher/default.sock
