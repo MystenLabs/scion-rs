b4c3f81f5c8d0e4cc1df5d03cfc54338b3b0e1d1
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2023-12-18 11:38:10 +0100

    feat: add path service implementation for static paths.

--- crates/scion/src/pan/path_service.rs
+++ crates/scion/src/pan/path_service.rs
@@ -1,14 +1,18 @@
 use bytes::Bytes;
+use chrono::Utc;
 use scion_proto::{address::IsdAsn, path::Path};
 
 #[derive(Debug, thiserror::Error)]
-pub enum PathLookupError {}
+pub enum PathLookupError {
+    #[error("no path available to destination")]
+    NoPath,
+}
 
 /// Trait for asynchronously retrieving paths to SCION ASes.
 #[async_trait::async_trait]
 pub trait AsyncPathService {
     /// Return a path to the specified AS.
-    async fn path_to(&self, scion_as: IsdAsn) -> Result<&Path, PathLookupError>;
+    async fn path_to<'a>(&'a self, scion_as: IsdAsn) -> Result<&'a Path, PathLookupError>;
 
     /// Provide the service with a path that it may choose to store.
     ///
@@ -27,4 +31,33 @@ pub trait AsyncPathService {
     ///
     /// See [`Self::maybe_add_shared_path`] for a variant which may avoid copying the path.
     fn maybe_add_path(&self, path: &Path<&mut [u8]>) -> bool;
+}
+
+#[async_trait::async_trait]
+impl AsyncPathService for Path<Bytes> {
+    /// Return a path to the specified AS.
+    async fn path_to(&self, scion_as: IsdAsn) -> Result<&Path, PathLookupError> {
+        if self.isd_asn.destination != scion_as {
+            return Err(PathLookupError::NoPath);
+        }
+        if let Some(metadata) = self.metadata.as_ref() {
+            if metadata.expiration < Utc::now() {
+                tracing::warn!(
+                    destination=%scion_as,
+                    path=?self,
+                    "attempted to send packet with expired, static path"
+                );
+                return Err(PathLookupError::NoPath);
+            }
+        }
+        Ok(self)
+    }
+
+    fn maybe_add_shared_path(&self, _path: &Path<Bytes>) -> bool {
+        false
+    }
+
+    fn maybe_add_path(&self, _path: &Path<&mut [u8]>) -> bool {
+        false
+    }
 }
